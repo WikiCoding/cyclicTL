a926a349b5ce17a9e3396242a62a27f17b4c9dad
Tiago Castro <121066161+WikiCoding@users.noreply.github.com>
2023-02-12 22:34:12 +0000

    Update startpage.js

--- public/startpage.js
+++ public/startpage.js
@@ -11,7 +11,7 @@
 //   //add condition for expired/invalid cookie
 // })
 
-const url = 'http://localhost:3000'
+const url = 'https://ill-pink-mite-veil.cyclic.app'
 
 const startPage = async () => {
   let result = await fetch(`${url}/`, {
