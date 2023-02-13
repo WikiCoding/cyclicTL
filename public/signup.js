c0549bec3387e11b727198d7502bccb7671eb61d
Tiago Castro <121066161+WikiCoding@users.noreply.github.com>
2023-02-13 12:31:43 +0000

    Update signup.js

--- public/signup.js
+++ public/signup.js
@@ -1,7 +1,7 @@
-const chk = document.querySelector('.chk');
-
 const url = 'https://cyclictl.cyclic.app';
 //const url = 'https://ill-pink-mite-veil.cyclic.app'
+
+const chk = document.querySelector('.chk');
 
 chk.addEventListener('click', () => {
   if (passInput.type === 'password') {
@@ -20,17 +20,16 @@ signup.addEventListener('click', async (e) => {
   e.preventDefault();
   const jsonBody = {};
   jsonBody.username = usernameInput.value;
-  //jsonBody.email = emailInput.value;
+
   jsonBody.password = passInput.value;
-  //jsonBody.tokens = [{ token: '' }];
-  console.log(jsonBody);
+
   let result = await fetch(`${url}/signup`, {
     method: 'POST',
     headers: { "content-type": "application/json" },
     body: JSON.stringify(jsonBody)
   });
   const data = await result.json();
-  console.log(data, 'from app');
+
   if (data !== undefined) {
     alert('User succesfully created!')
   }
