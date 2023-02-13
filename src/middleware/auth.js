d28caf73a90ddce51791d5e80573bb0b143a0dfb
Tiago Castro <121066161+WikiCoding@users.noreply.github.com>
2023-02-13 11:47:50 +0000

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,13 +3,12 @@ const User = require('../models/userModel');
 
 const auth = async (req, res, next) => {
   try {
-    //const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2U1ODNmNTU4ZWY3ZjljOWM1NmI3MmUiLCJpYXQiOjE2NzU5OTA1Mjh9.A8czgY3wbTBPal4ZooQWBJ54y8irALMHZcDw19B4PSo'
     const token = req.cookies['auth_token']; //bringing the token in the frontend from the cookies
-    //console.log(token);
     //const token = req.header('Authorization').replace('Bearer ', '');
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET, {
+      expiresIn: '1days'
+    });
     const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
-    //console.log(user);
 
     if (!user) {
       throw new Error();
@@ -21,9 +20,8 @@ const auth = async (req, res, next) => {
     next();
 
   } catch (e) {
-    // req.user = 'No user'
-    // next();
-    res.status(401).send({ error: 'Please authenticate.' });
+    //res.status(401).send({ error: 'Please authenticate.' });
+    res.redirect('/')
   }
 }
 
