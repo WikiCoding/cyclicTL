dcfba614d9d155ca659b4a42ff8d0b716fa353e6
Tiago Castro <121066161+WikiCoding@users.noreply.github.com>
2023-02-13 12:30:54 +0000

    Update startpage.js

--- public/startpage.js
+++ public/startpage.js
@@ -2,11 +2,12 @@
 const url = 'https://cyclictl.cyclic.app'
 
 const userLoggedIn = async () => {
-  const token = document.cookie.replace('auth_token=', '');
   const fetchUser = await fetch(`${url}/loggedUser`, {
     method: 'GET'
   })
   const loggedIn = await fetchUser.json();
+
+  const token = document.cookie.replace('auth_token=', '');
 
   if (loggedIn.tokens[0].token === token) {
     location.href = '/todos';
